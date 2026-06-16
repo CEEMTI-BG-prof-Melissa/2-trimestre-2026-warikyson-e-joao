ce8194f6e09ec0571d51d5d06dd5ca56c0b6fd03
joaohsctomaz-svg <joao.hsctomaz@aluno.edu.es.gov.br>
2026-06-16 11:10:22 -0300

    Create index.php

---
+++ aula01.php/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+    <head>
+        <meta charset="UT-8">
+        <title> aula 01 html </title>
+</head>
+
+    <body>
+        <?php
+            echo "Olá, <br> Mundo!";
+        ?>
+
+    </body>
+</html>
